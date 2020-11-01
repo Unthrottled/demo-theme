93fd2dfc0f5b1c8fb9b86a12a7615bc007cc466c
DATBOI <alexsimons9999@gmail.com>
2020-11-01 12:18:03 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.6.1"
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "io.unhrottled"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    // working version
+    version = "2020.2.2"
+
+    // broken version
+//    version = "LATEST-EAP-SNAPSHOT"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
